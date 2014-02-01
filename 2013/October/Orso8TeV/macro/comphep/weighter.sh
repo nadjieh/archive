53a01d5c71941588fb97447cedf0d2cc77e3abef
nadjieh <ajafari@cern.ch>
2014-02-01 20:29:46 +0330

    V01.02.2014

---
+++ 2013/October/Orso8TeV/macro/comphep/weighter.sh
@@ -0,0 +1,10 @@
+#./Plot prefix TreesEle_ out TreesEle_comphep_RW.root data comphep mcinput comphep beta 0.9038 mcname  comphep unc 0
+#./Plot prefix TreesEle_ out TreesEle_comphep_RW.root data comphep mcinput comphep beta 0.882883 mcname  comphep unc 0
+#./Plot prefix TreesMu_ out TreesMu_comphep_RW.root data comphep mcinput comphep beta 0.949631 mcname  comphep unc 0
+####real
+#emtw
+#./Plot prefix TreesEle_ out TreesEle_comphep_RW.root data comphep mcinput comphep beta 0.8341833 mcname  comphep unc 0
+#emet
+./Plot prefix TreesEle_ out TreesEle_comphep_RW.root data comphep mcinput comphep beta 0.867291 mcname  comphep unc 0
+#mu
+./Plot prefix TreesMu_ out TreesMu_comphep_RW.root data comphep mcinput comphep beta 0.92307331 mcname  comphep unc 0
